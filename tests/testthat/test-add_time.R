3a8fde3e428c9a7dcac44d62828cc170de3a7cac
Peter Winskill <p.winskill@imperial.ac.uk>
2022-08-17 11:15:53 +0100

    fix trailing timestep

--- tests/testthat/test-add_time.R
+++ tests/testthat/test-add_time.R
@@ -5,5 +5,5 @@ test_that("multiplication works", {
   interventions <- example_site$interventions
   p1 <- add_time(p = p0, interventions = interventions)
   expect_equal(p1$baseline_year, min(interventions$year))
-  expect_equal(p1$timesteps, 365 * (1 + max(interventions$year) - min(interventions$year)) - 1)
+  expect_equal(p1$timesteps, 365 * (1 + max(interventions$year) - min(interventions$year)))
 })
