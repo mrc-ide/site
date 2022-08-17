a583eef6688d9f7aff3d1fb70d441046ab5722e5
Peter Winskill <p.winskill@imperial.ac.uk>
2022-08-17 11:08:35 +0100

    fix trailing timestep

--- R/time.R
+++ R/time.R
@@ -1,5 +1,5 @@
 add_time <- function(p, interventions){
   p$baseline_year <- min(interventions$year)
-  p$timesteps <- (diff(range(interventions$year)) + 1) * 365
+  p$timesteps <- ((diff(range(interventions$year)) + 1) * 365) - 1
   return(p)
 }
