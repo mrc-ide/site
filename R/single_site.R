6c429d877dce378a5e550371ced71027b6ad7b08
Peter Winskill <p.winskill@imperial.ac.uk>
2022-08-19 08:41:03 +0100

    Add site info to single site

--- R/single_site.R
+++ R/single_site.R
@@ -4,13 +4,14 @@
 #' @param index Index row from site_file$sites
 #'
 #' @return Single site
+#' @export
 single_site <- function(site_file, index){
   if(index < 1 | index > nrow(site_file$sites)){
     stop("Mis-specified site index")
   }
   index_site <- site_file$sites[index, ]
 
-  to_mod <- c("interventions", "pyrethroid_resistance", "population",
+  to_mod <- c("sites", "interventions", "pyrethroid_resistance", "population",
               "vectors", "seasonality", "eir")
 
   site <- site_file
