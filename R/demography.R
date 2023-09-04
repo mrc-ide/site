0383f2c34eeddea32750fb6d9898a8e255c086d3
Peter Winskill <p.winskill@imperial.ac.uk>
2023-09-04 15:41:09 +0100

    ingest direct demography matrix

--- R/demography.R
+++ R/demography.R
@@ -7,19 +7,15 @@
 add_demography <- function(p, demography){
 
   # Age group upper
-  ages <- round(unique(demography$age_upper) * 365)
+  ages <- as.integer(colnames(demography))
   # Single demography currently
-  timesteps <- 365 * (unique(demography$year) - p$baseline_year)
-  # Take demography is first year as static demography
-  deathrates <- demography$mortality_rate / 365
-  # Create matrix of death rates
-  deathrates_matrix <- matrix(deathrates, nrow = length(timesteps), byrow = TRUE)
+  timesteps <- 365 * (as.integer(rownames(demography)) - p$baseline_year)
   # Add parameters
   p <- malariasimulation::set_demography(
     parameters = p,
     agegroups = ages,
     timesteps = timesteps,
-    deathrates = deathrates_matrix
+    deathrates = demography
   )
 
   return(p)
