e3d425cec3fea0bea7aac2d97d83ebfdaae7d3d9
Lydia Haile <l.haile@imperial.ac.uk>
2024-09-24 15:48:22 +0100

    updated pev function +test

--- tests/testthat/test-rtss.R
+++ tests/testthat/test-rtss.R
@@ -1,19 +1,21 @@
-test_that("adding rtss works", {
+test_that("adding pev works", {
   example_site <- single_site(example_site, 1)
   interventions <- example_site$interventions
-  interventions$rtss_cov[1:10] <- 0.5
+  interventions$coverage <- 0.5
+  interventions$vaccine<- 'RTS,S'
+  interventions$booster_coverage<- 0.8
   p0 <- malariasimulation::get_parameters()
   p0$baseline_year <- 2000
-  p1 <- add_rtss(
+  p1 <- add_pev_epi(
     p = p0,
     interventions = interventions)
 
   month <- 365 / 12
-  expect_equal(p1$rtss_epi_age, round(6 * month))
-  expect_equal(p1$rtss_epi_booster_coverage, 0.8)
-  expect_equal(p1$rtss_epi_boosters, round(18 * month))
-  expect_equal(p1$rtss_epi_timesteps, 1 + (365 * (interventions$year - p0$baseline_year)))
-  expect_equal(p1$rtss_epi_coverages, interventions$rtss_cov)
-  expect_equal(p1$rtss_epi_min_wait, 0)
-  expect_equal(p1$rtss_epi_seasonal_boosters, FALSE)
+  expect_equal(p1$pev_epi_age, round(6 * month))
+  expect_equal(mean(p1$pev_epi_booster_coverage), 0.8)
+  expect_equal(p1$pev_epi_booster_spacing, round(12 * month))
+  expect_equal(p1$pev_epi_timesteps, 1 + (365 * (interventions$year - p0$baseline_year)))
+  expect_equal(p1$pev_epi_coverages, interventions$coverage)
+  expect_equal(p1$pev_epi_min_wait, 0)
+  expect_equal(p1$pev_epi_seasonal_boosters, FALSE)
 })
