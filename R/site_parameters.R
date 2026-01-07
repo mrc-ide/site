288a86614db628b7e4aa1d4b22c5c47adf98c8f3
Peter Winskill <p.winskill@imperial.ac.uk>
2026-01-07 15:55:44 +0000

    Tidy paramet er draw

--- R/site_parameters.R
+++ R/site_parameters.R
@@ -14,14 +14,26 @@
 #'
 #' @return A malariasimulation parameter list
 #' @export
-site_parameters <- function(interventions, demography, vectors, seasonality,
-                            min_ages = c(0, 5, 15) * 365, species = "pf",
-                            eir = NULL, draw = NULL, overrides = list(), burnin = 0){
-
+site_parameters <- function(
+  interventions,
+  demography,
+  vectors,
+  seasonality,
+  min_ages = c(0, 5, 15) * 365,
+  species = "pf",
+  eir = NULL,
+  draw = NULL,
+  overrides = list(),
+  burnin = 0
+) {
   p <- malariasimulation::get_parameters(overrides = overrides)
 
+  if (!is.null(draw)) {
+    p <- malariasimulation::set_parameter_draw(p, draw)
+  }
+
   p$burnin <- 0
-  if(burnin > 0){
+  if (burnin > 0) {
     p$burnin <- burnin
     interventions <- burnin_interventions(interventions, burnin)
     demography <- burnin_demography(demography, burnin)
@@ -32,20 +44,12 @@ site_parameters <- function(interventions, demography, vectors, seasonality,
     add_seasonality(seasonality = seasonality) |>
     add_vectors(vectors = vectors) |>
     add_demography(demography = demography) |>
-    add_interventions(interventions = interventions,
-                      species = species) |>
+    add_interventions(interventions = interventions, species = species) |>
     set_age_outputs(min_ages = min_ages)
 
-  if(species == "pf") {
-    if(!is.null(draw)){
-      p <- malariasimulation::set_parameter_draw(p, draw)
-    }
-  }
-
-  if(!is.null(eir)){
+  if (!is.null(eir)) {
     p <- malariasimulation::set_equilibrium(p, init_EIR = eir)
   }
 
   return(p)
 }
-
