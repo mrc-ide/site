779c9ae1e865339983fbf4c9d31fa64ac3a6562e
Peter Winskill <p.winskill@imperial.ac.uk>
2026-01-19 15:25:10 +0000

    First pass testing of RTSS inputs age and hybrid options

--- R/vaccines.R
+++ R/vaccines.R
@@ -4,23 +4,31 @@
 #'
 #' @return modified parameter list
 add_rtss <- function(p, vaccine) {
-  # Format boosters
-  booster_cols <- intersect(
-    paste0("rtss_booster", 1:100, "_cov"),
-    names(vaccine$implementation)
-  )
-  # Take absolute booster coverages and convert so they are relative to primary coverage
-  rtss_booster_coverage <- as.matrix(vaccine$implementation[, booster_cols]) |>
-    sweep(1, vaccine$implementation$rtss_primary_cov, "/")
-  if (ncol(rtss_booster_coverage) != length(vaccine$schedule) + 3) {
-    stop(
-      "Number of RTS,S booster coverage columns and booster ages do not align"
-    )
+  if (vaccine$delivery == "age-based") {
+    n_boosters <- length(vaccine$booster_ages)
+    booster_spacing <- eg$interventions$vaccine$booster_spacing
+    min_wait = 0
+    seasonal_boosters <- FALSE
   }
-  booster_spacing <- setdiff(vaccine$schedule[3:length(vaccine$schedule)])
+
+  if (vaccine$delivery == "hybrid") {
+    n_boosters <- 1
+    booster_spacing <- vaccine$implementation$hybrid_booster_day_of_year
+    min_wait = 182
+    seasonal_boosters <- TRUE
+  }
+
+  # Booster coverage is input realative to primary series coverage
+  booster_coverage <- as.matrix(vaccine$implementation[, paste0(
+    "rtss_booster",
+    1:n_boosters,
+    "_cov"
+  )]) |>
+    sweep(1, vaccine$implementation$rtss_primary_cov, "/")
+
   booster_profile <- rep(
     malariasimulation::rtss_booster_profile,
-    length(booster_spacing)
+    n_boosters
   )
 
   p <- malariasimulation::set_pev_epi(
@@ -28,17 +36,18 @@ add_rtss <- function(p, vaccine) {
     profile = malariasimulation::rtss_profile,
     coverages = vaccine$implementation$rtss_primary_cov,
     timesteps = vaccine$implementation$vaccine_coverage_timesteps,
-    age = vaccine$schedule[1],
-    min_wait = 0,
+    age = vaccine$primary_schedule[1],
+    min_wait = min_wait,
     booster_spacing = booster_spacing,
-    booster_coverage = rtss_booster_coverage,
-    booster_profile = booster_profile
+    booster_coverage = booster_coverage,
+    booster_profile = booster_profile,
+    seasonal_boosters = seasonal_boosters
   )
 
   return(p)
 }
 
-#' Add RTS,S
+#' Add R21
 #'
 #' @inheritParams add_interventions
 #'
