599424725db24b73fcc2a005cf6a0580505f0f54
Peter Winskill <p.winskill@imperial.ac.uk>
2026-01-20 14:40:19 +0000

    Treatment refactor

---
+++ R/treatment.R
@@ -0,0 +1,78 @@
+#' Add Drugs
+#'
+#' @inheritParams add_interventions
+#'
+#' @return modified parameter list
+add_drugs <- function(p) {
+  # Shape (alpha) 2.516 and scale (beta) 46.68 From Gina's
+  # fit to Cisse et al data
+  SP_params <- c(0.9, 0.32, 2.516, 46.68)
+
+  # For treatment of clinical disease, we already include efficacy within
+  # The effective coverage, so any treatment has assumed 100% efficacy
+  SP_full_efficacy <- SP_params
+  SP_full_efficacy[1] <- 1
+  AL_full_efficacy <- malariasimulation::AL_params
+  AL_full_efficacy[1] <- 1
+
+  if (p$parasite == "falciparum") {
+    p <- malariasimulation::set_drugs(
+      parameters = p,
+      list(
+        SP_params,
+        malariasimulation::AL_params,
+        malariasimulation::SP_AQ_params,
+        SP_full_efficacy,
+        AL_full_efficacy
+      )
+    )
+  }
+  if (p$parasite == "vivax") {
+    p <- malariasimulation::set_drugs(
+      parameters = p,
+      list(
+        c(SP_params, 0, 0, 0),
+        c(malariasimulation::AL_params, 0, 0, 0),
+        c(malariasimulation::SP_AQ_params, 0, 0, 0),
+        c(SP_full_efficacy, 0, 0, 0),
+        c(AL_full_efficacy, 0, 0, 0)
+      )
+    )
+  }
+  return(p)
+}
+
+#' Add treatment
+#'
+#' @inheritParams add_interventions
+#'
+#' @return modified parameter list
+add_treatment <- function(p, treatment) {
+  # Assuming treatent changes happen on January 1st
+  timesteps <- calendar_to_timestep(
+    year = treatment$implementation$year,
+    start_year = p$start_year
+  )
+
+  non_act_coverage <- treatment$implementation$tx_cov *
+    (1 - treatment$implementation$prop_act)
+  act_coverage <- treatment$implementation$tx_cov *
+    treatment$implementation$prop_act
+
+  # Non ACT (SP)
+  p <- malariasimulation::set_clinical_treatment(
+    parameters = p,
+    drug = 4,
+    timesteps = timesteps,
+    coverages = non_act_coverage
+  )
+  # ACT (AL)
+  p <- malariasimulation::set_clinical_treatment(
+    parameters = p,
+    drug = 5,
+    timesteps = timesteps,
+    coverages = act_coverage
+  )
+
+  return(p)
+}
--- tests/testthat/test-treatment.R
+++ tests/testthat/test-treatment.R
@@ -1,27 +1,96 @@
-test_that("adding treatment works", {
-  single_site <- subset_site(example_site, example_site$eir[1,])
-  interventions <- single_site$interventions
-  p0 <- malariasimulation::get_parameters()
-  p0$baseline_year <- 2000
+p0 <- malariasimulation::get_parameters()
+p0$start_year <- 2000
+
+test_that("adding drugs works for falciparum", {
   # Drug types
-  p0 <- add_drugs(p0)
-
-  p1 <- add_treatment(
-    p = p0,
-    interventions = interventions
+  p1 <- add_drugs(p0)
+  expect_equal(
+    p1$drug_efficacy,
+    c(0.90, 0.95, 0.90, 1.00, 1.00)
   )
-
-  expect_equal(p1$clinical_treatment_drugs, list(4, 5))
-
-  expect_equal(p1$clinical_treatment_coverages[[1]], interventions$tx_cov * (1 - interventions$prop_act))
-  expect_equal(p1$clinical_treatment_coverages[[2]], interventions$tx_cov * interventions$prop_act)
-
-
-  expect_equal(p1$clinical_treatment_timesteps,
-               list(
-                 1 + 365 * (interventions$year - p0$baseline_year),
-                 1 + 365 * (interventions$year - p0$baseline_year)
-               )
+  expect_equal(
+    p1$drug_rel_c,
+    c(0.32000, 0.05094, 0.32000, 0.32000, 0.05094)
+  )
+  expect_equal(
+    p1$drug_prophylaxis_shape,
+    c(2.516, 11.300, 4.300, 2.516, 11.300)
+  )
+  expect_equal(
+    p1$drug_prophylaxis_scale,
+    c(46.68, 10.6, 38.1, 46.68, 10.6)
   )
 })
 
+test_that("adding drugs works for vivax", {
+  # Drug types
+  p0$parasite <- "vivax"
+  p1 <- add_drugs(p0)
+  expect_equal(
+    p1$drug_efficacy,
+    c(0.90, 0.95, 0.90, 1.00, 1.00)
+  )
+  expect_equal(
+    p1$drug_rel_c,
+    c(0.32000, 0.05094, 0.32000, 0.32000, 0.05094)
+  )
+  expect_equal(
+    p1$drug_prophylaxis_shape,
+    c(2.516, 11.300, 4.300, 2.516, 11.300)
+  )
+  expect_equal(
+    p1$drug_prophylaxis_scale,
+    c(46.68, 10.6, 38.1, 46.68, 10.6)
+  )
+  expect_equal(
+    p1$drug_hypnozoite_efficacy,
+    rep(0, 5)
+  )
+  expect_equal(
+    p1$drug_hypnozoite_prophylaxis_shape,
+    rep(0, 5)
+  )
+  expect_equal(
+    p1$drug_hypnozoite_prophylaxis_scale,
+    rep(0, 5)
+  )
+})
+
+example_treatment <- list(
+  implementation = data.frame(
+    name = "place",
+    year = 2000:2002,
+    tx_cov = 0.1,
+    prop_act = 0.2
+  )
+)
+
+test_that("Setting treatment works", {
+  p1 <- add_drugs(p0) |>
+    add_treatment(treatment = example_treatment)
+
+  expect_equal(
+    p1$clinical_treatment_coverages[[1]],
+    example_treatment$implementation$tx_cov *
+      (1 - example_treatment$implementation$prop_act)
+  )
+  expect_equal(
+    p1$clinical_treatment_coverages[[2]],
+    example_treatment$implementation$tx_cov *
+      example_treatment$implementation$prop_act
+  )
+  expect_equal(
+    p1$clinical_treatment_timesteps[[1]],
+    calendar_to_timestep(
+      year = example_treatment$implementation$year,
+      start_year = p1$start_year
+    )
+  )
+  expect_equal(
+    p1$clinical_treatment_timesteps[[2]],
+    calendar_to_timestep(
+      year = example_treatment$implementation$year,
+      start_year = p1$start_year
+    )
+  )
+})
